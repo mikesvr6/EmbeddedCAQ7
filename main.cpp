a9b1156820efbcf3455b71278107bc7a3b2fe3be
mikesvr6 <michaeloceallachain@gmail.com>
2024-07-14 14:32:48 +0000

    Step 2 - not successfull, blinks do not toggle from led 1 to led 2

--- main.cpp
+++ main.cpp
@@ -4,15 +4,17 @@ DigitalOut led1(LED1);
 DigitalOut led2(LED2);
 
 // Blinking rate in milliseconds
-#define BLINKING_RATE     500ms
-
+#define BLINKING_RATE_A     5000ms
+#define BLINKING_RATE_B     2000ms
 
 // main() runs in its own thread in the OS
 int main()
 {
     while (true) {
-        led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
+        led1 = !led1;
+        ThisThread::sleep_for(BLINKING_RATE_A);
+        led2 = !led2;
+        ThisThread::sleep_for(BLINKING_RATE_B);
     }
 }
 
