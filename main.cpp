094fa2d37dfa9e16621a6f9cc025d6c7513356fa
mikesvr6 <michaeloceallachain@gmail.com>
2024-07-14 14:15:35 +0000

    step 2 - blink idea

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include "mbed.h"
 
+DigitalOut led1(LED1);
+DigitalOut led2(LED2);
+
+// Blinking rate in milliseconds
+#define BLINKING_RATE     500ms
+
+
 // main() runs in its own thread in the OS
 int main()
 {
     while (true) {
-
+        led = !led;
+        ThisThread::sleep_for(BLINKING_RATE);
     }
 }
 
